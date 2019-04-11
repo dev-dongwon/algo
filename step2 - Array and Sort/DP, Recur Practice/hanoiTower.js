5ae787bd4e372fb09013c968b214e37c1cafa725
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-11 14:41:28 +0900

    하노이의 탑 recursion으로 구현 : javascipt
    recursive 이해를 위해 재귀로 구현

---
+++ step2 - Array and Sort/DP, Recur Practice/hanoiTower.js
@@ -0,0 +1,18 @@
+// 하노이의 탑 : 두 가지 규칙이 있다
+
+// 1. 한 번에 하나의 원반만 움직일 수 있습니다.
+// 2. 자신보다 작은 원반이 그 아래에 놓일 수 없습니다.
+//    예를 들어 원반 3이 축에 있다면 원반 3 밑에 있는 원반은 모두 3보다 큰 숫자로 되어 있어야 합니다.
+
+const hanoi = function(n, from, to, extra, count) {
+    if (n == 1)
+        console.log(`${from}에서 ${to}로 이동!`)
+        
+    if (n > 1) {
+        hanoi(n-1, from, extra, to, count++);
+        hanoi(1, from, to, extra, count++);
+        hanoi(n-1, extra, to, from, count++);
+    }
+}
+    
+hanoi(3, "A", "B", "C", 1);
